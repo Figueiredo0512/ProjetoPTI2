5cda1edcf323254cdb6342e445783f4ce272e906
Matheus Figueiredo <matheus.00000@gmail.com>
2022-08-22 07:30:30 -0300

    MVC login

---
+++ ProjetoPTI2/ProjetoPTI2/Controllers/LoginsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoPTI2.Models;
+using System.Collections.Generic;
+
+namespace ProjetoPTI2.Controllers
+{
+    public class LoginsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjetoPTI2/ProjetoPTI2/Models/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjetoPTI2.Models
+{
+    public class Login
+    {
+        public int User { get; set; }
+        public int Email { get; set; }
+    }
+}
