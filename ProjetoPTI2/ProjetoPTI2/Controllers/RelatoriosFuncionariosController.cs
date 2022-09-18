11d034a1bffb1462b32094653ffa8639c15fa551
Matheus Figueiredo <matheus.00000@gmail.com>
2022-09-18 19:54:06 -0300

    criação telas de relatorio

---
+++ ProjetoPTI2/ProjetoPTI2/Controllers/RelatoriosFuncionariosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoPTI2.Controllers
+{
+	public class RelatoriosFuncionariosController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
